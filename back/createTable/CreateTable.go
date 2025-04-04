e23174e29705bc5684881a0ae7871c46234ab252
amir <cavalier7654@gmail.com>
2025-04-04 20:07:07 +0330

    create sqlite as db

--- back/createTable/CreateTable.go
+++ back/createTable/CreateTable.go
@@ -1,1 +1,16 @@
 package createTable
+
+import "database/sql"
+
+func CreateTable(db *sql.DB) {
+
+	q := `CREATE TABLE IF NOT EXISTS todos (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		title TEXT NOT NULL
+	);`
+
+	_, err := db.Exec(q)
+	if err != nil {
+		panic(err)
+	}
+}
--- back/main.go
+++ back/main.go
@@ -2,9 +2,30 @@ package main
 
 import (
 	"back/Handler"
+	"back/createTable"
+	"database/sql"
+	_ "github.com/glebarez/sqlite"
 	"net/http"
 )
 
 func main() {
+	db, err := sql.Open("sqlite", "./DB/todo.db")
+	if err != nil {
+		panic(err)
+	}
+
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(db)
+
+	createTable.CreateTable(db)
+
 	http.HandleFunc("/", Handler.Handler)
+	err = http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
 }
