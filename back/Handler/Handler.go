b9b272532ad55495fd6157e27b1a7ffe61625181
amir <cavalier7654@gmail.com>
2025-04-10 12:26:15 +0330

    crud: post

--- back/Handler/Handler.go
+++ back/Handler/Handler.go
@@ -43,7 +43,7 @@ func GetHandler(db *sql.DB) gin.HandlerFunc {
 
 		c.JSON(http.StatusOK, todos)
 	}
-}
+} // ✅
 
 // GetHandlerById Get by ID
 func GetHandlerById(db *sql.DB) gin.HandlerFunc {
@@ -65,14 +65,37 @@ func GetHandlerById(db *sql.DB) gin.HandlerFunc {
 
 		c.JSON(http.StatusOK, todo)
 	}
-}
+} // ✅
 
 // PostHandler post
 func PostHandler(db *sql.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		//codes
+		var todo Todos
+		err := c.ShouldBindJSON(&todo)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+			return
+		}
+
+		//err = checkTodo(&todo)
+		//if err != nil {
+		//	c.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		//	return
+		//}
+
+		result, err := db.Exec("INSERT INTO todos (title) VALUES (?)", todo.Title)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+			return
+		}
+
+		ourId, _ := result.LastInsertId()
+		todo.ID = int(ourId)
+
+		c.JSON(http.StatusCreated, todo)
+
 	}
-}
+} // ✅
 
 // PutHandler update
 func PutHandler(db *sql.DB) gin.HandlerFunc {
@@ -102,4 +125,4 @@ func DeleteHandler(db *sql.DB) gin.HandlerFunc {
 		}
 		c.JSON(http.StatusOK, gin.H{"message": "deleted successfully"})
 	}
-}
+} // ✅
